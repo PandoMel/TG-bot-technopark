e13b6a39d0a56c5c2594209f87e82b95147a18d8
PandoMel <sh1psochi@gmail.com>
2026-03-04 15:54:17 +0300

    Update fallback.py

--- routers/fallback.py
+++ routers/fallback.py
@@ -1,18 +1,22 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 
 router = Router()
 
 
 @router.message(F.text)
-async def lovim_text(message: types.Message, state: FSMContext):
+async def lovim_text(message: Message, state: FSMContext):
     await state.clear()
-    await message.answer('Ошибка. Отправляйте данные согласно диалогу.\nИспользуйте команду: /start')
+    await message.answer(
+        'Ошибка. Отправляйте данные согласно диалогу.\nИспользуйте команду: /start'
+    )
 
 
-# На случай “битых” callback_data или устаревших кнопок
 @router.callback_query()
 async def fallback_callback(call: CallbackQuery):
-
-    await call.answer("Кнопка неактуальна или действие недоступно.", show_alert=True)
+    await call.answer(
+        "Кнопка неактуальна или действие недоступно.",
+        show_alert=True
+    )
