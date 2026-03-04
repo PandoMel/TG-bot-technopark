fae68013c981e5908fda64c5ab98be3906d2fe70
PandoMel <sh1psochi@gmail.com>
2026-03-04 14:24:45 +0300

    Create fallback.py

---
+++ routers/fallback.py
@@ -0,0 +1,18 @@
+from aiogram import Router
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import Command
+
+router = Router()
+
+
+@router.message(F.text)
+async def lovim_text(message: types.Message, state: FSMContext):
+    await state.clear()
+    await message.answer('Ошибка. Отправляйте данные согласно диалогу.\nИспользуйте команду: /start')
+
+
+# На случай “битых” callback_data или устаревших кнопок
+@router.callback_query()
+async def fallback_callback(call: CallbackQuery):
+
+    await call.answer("Кнопка неактуальна или действие недоступно.", show_alert=True)
